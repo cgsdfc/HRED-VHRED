5012c9ac02a46cf79ef66ce918567d7440e9149f
cgsdfc <cgsdfc@126.com>
2019-04-27 00:04:55 +0800

    Create Extractor API
    Extract data from docker container' logfile.

---
+++ monitor/extract_stats2.py
@@ -0,0 +1,9 @@
+import extractor
+from extractor import Field
+
+if __name__ == '__main__':
+    extractor.main([
+        Field('epoch', r'Epoch: (\d+)'),
+        Field('acc_word_ppl', r'acc_word_perplexity = ([\d.]+)'),
+        Field('cur_word_ppl', r'cur_word_perplexity = ([\d.]+)'),
+    ])
