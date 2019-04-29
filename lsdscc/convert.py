f4207e088c581036a557cbe1d49c6958f9aae9b5
cgsdfc <cgsdfc@126.com>
2019-04-29 20:25:16 +0800

    Minor fixes to lsdscc data makers.

--- lsdscc/convert.py
+++ lsdscc/convert.py
@@ -8,22 +8,22 @@ import numpy as np
 
 LSDSCC_SEP = '<EOS>#TAB#'
 LSDSCC_SEP_RE = re.compile(LSDSCC_SEP)
-EOS = '</s>'
-EOS = EOS.join('  ')
 
-UNKNOWN = 'unknown'
-UNKNOWN_RE = re.compile(UNKNOWN)
+# The desired EOS token.
+EOS = '</s>'.join(' ' * 2)
+
+LSDSCC_UNK = 'unknown'
+LSDSCC_UNK_RE = re.compile(LSDSCC_UNK)
 UNK = '<unk>'
 
-CONVERTER = 'convert_text2dict.py'
 SUFFIX = '.dialogues.pkl'
-DEF_INPUT = '/home/cgsdfc/LSDSCC-Reddit-Movie/dataset.txt'
 
+# Consts from the paper. Used to verify the actual data.
 RESPONSE_MAX_LEN = 60
 QUERY_MAX_LEN = 100
 
 
-def replace_eos_on_the_whole_file(input, output, verbose):
+def normalize(input, output, verbose):
     def check_max_len(query, response):
         if len(query.split()) > QUERY_MAX_LEN:
             logging.info('bad query: %s', query)
@@ -38,7 +38,7 @@ def replace_eos_on_the_whole_file(input, output, verbose):
                     check_max_len(query, response)
 
                 line = EOS.join((query, response))
-                line = UNKNOWN_RE.sub(UNK, line)
+                line = LSDSCC_UNK_RE.sub(UNK, line)
                 yield line
 
     with open(output, 'w') as out:
@@ -48,7 +48,7 @@ def replace_eos_on_the_whole_file(input, output, verbose):
 
 def split_train_test(filename):
     filename = Path(filename)
-    output_dir: Path = filename.parent
+    output_dir = filename.parent
     logging.info('input_file: %s', filename)
     logging.info('output_dir: %s', output_dir)
 
@@ -69,6 +69,7 @@ def split_train_test(filename):
         size = round(percent * len(data) / 100)
         chunk = data[offset: offset + size]
         logging.info('%d dialogues write to %s', len(chunk), filename)
+
         with open(filename, 'wb') as f:
             pickle.dump(chunk, f)
         offset += size
@@ -76,16 +77,17 @@ def split_train_test(filename):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', default=DEF_INPUT)
-    parser.add_argument('--output')
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--output', required=True)
     parser.add_argument('-r', '--replace', action='store_true', help='replace <eos> and <unk> token in input')
     parser.add_argument('-s', '--split', action='store_true', help='split a big dialogues pickle file into 3 files')
     parser.add_argument('-v', '--verbose')
 
-    logging.basicConfig(level=logging.INFO)
     args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
 
     if args.replace:
-        replace_eos_on_the_whole_file(args.input, args.output, args.verbose)
+        normalize(args.input, args.output, args.verbose)
     elif args.split:
         split_train_test(args.input)
--- lsdscc/make_data.sh
+++ lsdscc/make_data.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-INPUT_X=/home/cgsdfc/LSDSCC-Reddit-Movie/dataset.txt
+RAW_INPUT=/home/cgsdfc/LSDSCC-Reddit-Movie/dataset.txt
 INPUT=/home/cgsdfc/Serban_LSDSCC/dataset.txt.words
 OUTPUT=/home/cgsdfc/Serban_LSDSCC/Dataset
-CUTOFF=51200
+CUTOFF=51200 # Value from the paper
 
 # Normalize to required format.
-python lsdscc/convert.py -r --input $INPUT_X --output $INPUT
+python lsdscc/convert.py -r --input $RAW_INPUT --output $INPUT
 
 # Generate dictionary and one big dialogues file.
 python bin/convert_text2dict.py $INPUT $OUTPUT --cutoff $CUTOFF
