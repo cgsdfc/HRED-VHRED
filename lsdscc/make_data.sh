a97791c606c6cf45ea82492f6cff13f599166c57
cgsdfc <cgsdfc@126.com>
2019-04-30 16:50:20 +0800

    add indent to score json file.
    Readable is a primary goal.

--- lsdscc/make_data.sh
+++ lsdscc/make_data.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
 RAW_INPUT=/home/cgsdfc/LSDSCC-Reddit-Movie/dataset.txt
-INPUT=/home/cgsdfc/Serban_LSDSCC/dataset.txt.words
-OUTPUT=/home/cgsdfc/Serban_LSDSCC/Dataset
-CUTOFF=51200 # Value from the paper
+INPUT=/home/cgsdfc/SerbanLSDSCC/dataset.txt.words
+OUTPUT=/home/cgsdfc/SerbanLSDSCC/Train
+CUTOFF=30000
 
 # Normalize to required format.
 python lsdscc/convert.py -r --input $RAW_INPUT --output $INPUT
 
 # Generate dictionary and one big dialogues file.
 python bin/convert_text2dict.py $INPUT $OUTPUT --cutoff $CUTOFF
-
-INPUT=/home/cgsdfc/Serban_LSDSCC/Dataset.dialogues.pkl
-
-# Split the big dialogues into train, valid and test files.
-python lsdscc/convert.py -s --input $INPUT
