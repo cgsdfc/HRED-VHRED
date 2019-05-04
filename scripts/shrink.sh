dc9867e3ea9c01b3a2d82256cb2a78c9d272a735
cgsdfc <cgsdfc@126.com>
2019-05-04 11:12:58 +0800

    shrink opensub test set.

---
+++ scripts/shrink.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python scripts/shrink_opensub_test_data.py \
+    -c /home/cgsdfc/SerbanOpenSubData/dialogue_length3_6/test.context.txt \
+    -r /home/cgsdfc/SerbanOpenSubData/dialogue_length3_6/test.response.txt \
+    -p /home/cgsdfc/SerbanOpenSubData/dialogue_length3_6/eval
---
+++ scripts/shrink_opensub_test_data.py
@@ -0,0 +1,51 @@
+"""
+The opensub test set has 1M example, which is way to large for testing purpose.
+It runs on  a GPU for nearly 2 days.
+"""
+
+import argparse
+from pathlib import Path
+import logging
+
+import pandas as pd
+
+DEF_FRAC = 0.01
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', help='output dir')
+    parser.add_argument('-c', dest='context_file')
+    parser.add_argument('-r', dest='reference_file')
+    parser.add_argument('-f', dest='frac', type=float,
+                        help='fraction of samples', default=DEF_FRAC)
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+
+    output_dir = Path(args.p)
+
+
+    def load_file(path):
+        logging.info('loading %s', path)
+        return Path(path).read_text().splitlines()
+
+
+    def save_file(input_file, series):
+        output = output_dir.joinpath(Path(input_file).name)
+        logging.info('saving to %s', output)
+        output.write_text('\n'.join(series))
+
+
+    payload = {
+        'contexts': load_file(args.context_file),
+        'references': load_file(args.reference_file),
+    }
+
+    logging.info('loading data frame')
+    df = pd.DataFrame(data=payload)
+    print(df.head())
+
+    df = df.sample(frac=args.frac)
+    logging.info('samples: %d', len(df))
+
+    save_file(args.context_file, df['contexts'])
+    save_file(args.reference_file, df['references'])
